62b6c8e778f1f9ee908473f2c6dd771b6bf3360d
mac company <m15577822957@163.com>
2019-11-20 15:31:08 +0800

    111

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,24 +1,12 @@
 const { router } = require('../../framework/index.js');
 
 Page({
-  // onShareAppMessage() {
-  //   return config.shareData;
-  // },
   onReady() {
     const arr = [1, 2, 3].map((item) => ({ id: item }));
     arr.map((obj) => {
       obj.name = '111';
       return obj;
     });
-    const obj = {
-      b: {
-        name: 'Ming',
-      },
-    };
-    const clone = this.deepCopyTwo(obj);
-    console.log(clone);
-    obj.b.name = '2222';
-    console.log(clone);
   },
   navigate: router.navigate,
   queryTo() {
@@ -39,20 +27,5 @@ Page({
         name: 'Anny',
       },
     });
-  },
-  deepCopyTwo(obj) {
-    const that = this;
-    const objClone = Array.isArray(obj) ? [] : {};
-    if (obj && typeof obj === 'object') {
-      obj.forEach((item, index) => {
-        if (typeof item === 'object') {
-          objClone[index] = that.deepCopyTwo(item);
-        } else {
-          // 如果不是，简单复制
-          objClone[index] = item;
-        }
-      });
-    }
-    return objClone;
   },
 });
