4d0e55b32fa8774969d9ba61be7535909ba300c0
mac company <m15577822957@163.com>
2019-11-20 15:05:49 +0800

    test

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,6 +4,13 @@ Page({
   // onShareAppMessage() {
   //   return config.shareData;
   // },
+  onReady() {
+    const arr = [1,2,3].map(item => ({id: item,}));
+    arr.map(obj => {
+      obj.name = '111';
+      return obj;
+    });
+  },
   navigate: router.navigate,
   queryTo() {
     // wx.navigateTo({
@@ -23,5 +30,21 @@ Page({
         name: 'Anny',
       },
     })
+  },
+  deepCopyTwo(obj) {
+    const that = this;
+    let objClone = Array.isArray(obj) ? [] : {};
+    if (obj && typeof obj == 'object') {
+        for (const key in obj) {
+            //判断obj子元素是否为对象，如果是，递归复制
+            if (obj[key] && typeof obj[key] === "object") {
+                objClone[key] = that.deepCopyTwo(obj[key]);
+            } else {
+                //如果不是，简单复制
+                objClone[key] = obj[key];
+            }
+        }
+    }
+    return objClone;
   },
 });
