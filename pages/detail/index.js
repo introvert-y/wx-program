2a28795c476246d43d0b8245f879c63a6476631b
mac company <m15577822957@163.com>
2019-12-10 17:13:19 +0800

    自定义导航栏

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,6 +1,15 @@
 // const { config } = require('../../framework/index.js');
 
 Page({
+  data: {
+    distance: 0,
+  },
+  onReady() {
+    this.setData({
+      distance: wx.getSystemInfoSync().statusBarHeight,
+    });
+    console.log(wx.getSystemInfoSync().statusBarHeight);
+  },
   // onShareAppMessage() {
   //   return config.shareData;
   // },
