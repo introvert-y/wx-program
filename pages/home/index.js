5d14a48d045bf86d7f2f2ab463c76f789a7e34f9
mac company <m15577822957@163.com>
2019-12-10 10:38:29 +0800

    slide动画

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,6 +3,10 @@ const { router, config, req } = require('../../framework/index.js');
 Page({
   data: {
     faq: config.faqTable[15],
+    isShow: false,
+    startX: 0,
+    endX: 0,
+    animationData: {},
   },
   onReady() {
     // this.getList();
@@ -13,6 +17,37 @@ Page({
       console.log(r1);
     });
   },
+  touchstart(e) {
+    this.setData({
+      startX: e.touches[0].clientX,
+    });
+    // console.log('touchstart', e.touches[0].clientX);
+  },
+  touchend(e) {
+    const { startX, isShow } = this.data;
+    // console.log('touchend', e.changedTouches[0].clientX);
+    if (startX - e.changedTouches[0].clientX > 0 && !isShow) {
+      console.log('显示');
+      this.Show();
+    } else if (startX - e.changedTouches[0].clientX < 0 && isShow) {
+      console.log('隐藏');
+      this.hide();
+    }
+    this.setData({
+      endX: e.changedTouches[0].clientX,
+    });
+  },
+  Show() {
+    this.setData({
+      isShow: true,
+    });
+  },
+  hide() {
+    this.setData({
+      isShow: false,
+    });
+  },
+
   queryTo() {
     // wx.navigateTo({
     //   url: '/pages/message/index',
