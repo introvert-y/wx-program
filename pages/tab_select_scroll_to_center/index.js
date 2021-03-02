3dd1f2ba5bd4dae4e4ec3b72240f7655644c2227
apple <apple@192.168.10.118>
2021-03-02 18:57:00 +0800

    优化选中标题居中显示

--- pages/tab_select_scroll_to_center/index.js
+++ pages/tab_select_scroll_to_center/index.js
@@ -46,6 +46,8 @@ Page({
   ],
     deviceWidth: 0,
     scrollLeft: 0,
+    bottomLineWidth: 0,
+    crtTab: 0,
   },
   onShow() {
     // 获取当前设备的宽度
@@ -57,26 +59,25 @@ Page({
     return config.shareData;
   },
   onMove(e) {
+    console.log('e.currentTarget', e.currentTarget);
+    const { index } = e.currentTarget.dataset;
     // 创建节点
     var query = wx.createSelectorQuery();
     //选择id
     var that = this;
+
     var width = 0
+
     // 获取当前点击元素的宽度
-    query.select('#item_' + e.currentTarget.dataset.index).boundingClientRect(function (rect) {
-      width = rect.width;
-      // 激活当前选中项
-      that.data.tabmenu.forEach(function (element, index) {
-        element.active = false
-        if (e.currentTarget.dataset.index == index) {
-          element.active = true
-        }
-      })
-      console.log('e.currentTarget.offsetLeft', e.currentTarget.offsetLeft, width / 2);
+    query.select('#item_' + index).boundingClientRect(function (rect) {
+
+      const clientWidth = rect.width;
+      console.log('e.currentTarget.offsetLeft', rect);
       that.setData({
-        tabmenu: that.data.tabmenu,
         // 设置选中元素居中显示
-        scrollLeft: e.currentTarget.offsetLeft + (width / 2 - that.data.deviceWidth / 2)
+        scrollLeft: e.currentTarget.offsetLeft + (clientWidth / 2 - that.data.deviceWidth / 2),
+        bottomLineWidth: clientWidth,
+        crtTab: index,
       })
     }).exec();
   },
